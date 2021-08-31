057c1d5ec2a4d89783df5e6db56bf632de77ea2c
jeffersonbaf <jeffersonbittencourt1512@gmail.com>
2021-08-31 13:50:54 -0300

    adiciona segundo programa feito em csharp

---
+++ csharp/segundoAplicativo/EstruturasCondicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Um diretor de uma empresa quer saber se os seus funcionarios estao gostando da empresa, porem caso alguem nao goste
+            // ele quer saber o motivo. UM FUNCIONARIO
+
+            string situacaoFuncionario;
+            string respostaSeNao;
+
+            Console.WriteLine("Você está satisfeito com o trabalho aqui? (Sim/Nao)");
+            situacaoFuncionario = Console.ReadLine();
+
+            if (situacaoFuncionario.ToLower() == "sim") // Para impedir conflito entre palavras maiuscula e minuscula, ele basicamente recebe tudo em minusculo.
+            {
+                Console.WriteLine ("Que legal, bom saber que você está satisfeito com a empresa!");
+            }
+            else if (situacaoFuncionario.ToLower() == "nao")
+            {
+                Console.WriteLine ("Por que você não está gostando de trabalhar em nossa empresa?");
+                respostaSeNao = Console.ReadLine();
+                Console.WriteLine ("A sua resposta é: " + respostaSeNao);
+            }
+            else
+            {
+                Console.WriteLine ("Opção inválida!");
+            }
+        Console.ReadKey();
+        }
+    }
+}
