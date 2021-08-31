5e2d8ed9ee00182d80e315ad6bf5760694ab5395
jeffersonbaf <jeffersonbittencourt1512@gmail.com>
2021-08-31 13:10:24 -0300

    adiciona pasta csharp

---
+++ csharp/primeiroAplicativo/primeiroAplicativo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeiroAplicativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Condicao;
+            Console.WriteLine("Hello Word"); //WriteLine escreve na linha de baixo, meio que uma quebra de linha imbutida no comando Write
+            Console.WriteLine("Como estás?");
+            Condicao = Console.ReadLine();
+
+            Console.WriteLine("A sua condição é: " + Condicao); // + esta juntando uma string com outra string
+            
+
+            Console.ReadKey(); //Serve para parar o console, esperando que alguma tecla seja apertada para o programa encerrar
+        }
+    }
+}
